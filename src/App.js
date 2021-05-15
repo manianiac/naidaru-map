c853ec14f73994bef0c6c631f7be6351129c8ee9
Will Barron <williambarronnexus@gmail.com>
2021-05-14 20:53:59 -0400

    initial map creation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
+import { CRS } from "leaflet";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapContainer
+        center={[-90, 128]}
+        minZoom={0}
+        zoom={2}
+        scrollWheelZoom={true}
+        style={{ height: window.innerHeight, width: window.innerWidth }}
+        crs={CRS.Simple}
+        maxZoom={7}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="/tiles/{z}/{x}/{y}.png"
+          noWrap={true}
+        />
+        <EventComponent />
+      </MapContainer>
     </div>
   );
 }
 
 export default App;
+const EventComponent = () => {
+  const map = useMapEvents({
+    click: (e) => {
+      console.log(e.latlng);
+    },
+  });
+  return null;
+};
